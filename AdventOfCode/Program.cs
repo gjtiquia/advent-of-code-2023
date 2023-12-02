c4594ddf7926ee96b281f839c1582029081a1020
GJ Tiquia <gershom.tiquia@gmail.com>
2023-12-02 16:28:04 +0800

    feat: finish part 2!

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,4 +1,3 @@
 ﻿string[] lines = File.ReadAllLines("./AdventOfCode/Day2/input.txt");
-string configuration = "12 red, 13 green, 14 blue";
 
-Console.WriteLine(Day2Parser.GetSumOfPossibleGameIDs(configuration, lines));
+Console.WriteLine(Day2Parser.GetSumOfPowerOfGames(lines));
