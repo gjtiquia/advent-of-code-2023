bd439ada9c25ee0da895e2690ccc9659215ebfc4
GJ Tiquia <gershom.tiquia@gmail.com>
2023-12-02 17:06:57 +0800

    feat: added all solutions in Program.cs

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,14 @@
-﻿string[] lines = File.ReadAllLines("./AdventOfCode/Day2/input.txt");
+﻿string baseDirectory = "./AdventOfCode";
 
-Console.WriteLine(Day2Parser.GetSumOfPowerOfGames(lines));
+Console.WriteLine("===== ADVENT OF CODE 2023 =====");
+
+// Day 1
+string[] day1Input = File.ReadAllLines($"{baseDirectory}/Day1/input.txt");
+Console.WriteLine($"Day 1 Part 1: (Invalid due to change of requirements in Part 2)");
+Console.WriteLine($"Day 1 Part 2: {Day1Parser.GetSumOfCalibrationValue(day1Input)}");
+
+// Day 2
+string[] day2Input = File.ReadAllLines($"{baseDirectory}/Day2/input.txt");
+string day2Part1Configuraion = "12 red, 13 green, 14 blue";
+Console.WriteLine($"Day 2 Part 1: {Day2Parser.GetSumOfPossibleGameIDs(day2Part1Configuraion, day2Input)}");
+Console.WriteLine($"Day 2 Part 2: {Day2Parser.GetSumOfPowerOfGames(day2Input)}");
