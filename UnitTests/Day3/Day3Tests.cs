41cc30e4a560a46a311f4066cbcde90f1814a952
GJ Tiquia <gershom.tiquia@gmail.com>
2023-12-04 10:06:09 +0800

    feat: find indexes of symbols

--- UnitTests/Day3/Day3Tests.cs
+++ UnitTests/Day3/Day3Tests.cs
@@ -2,5 +2,31 @@ namespace UnitTests;
 
 public class Day3Tests
 {
+    [TestCase("467..114..", new int[] { })]
+    [TestCase("...*......", new int[] { 3 })]
+    [TestCase("...$.*....", new int[] { 3, 5 })]
+    public void ShouldFindTheIndexesOfSymbols(string line, int[] targetIndexes)
+    {
+        int[] indexes = Day3Parser.FindIndexesOfSymbols(line);
+        Assert.That(indexes, Is.EqualTo(targetIndexes));
+    }
 
+
+}
+
+public class Day3Parser
+{
+    public static int[] FindIndexesOfSymbols(string line)
+    {
+        List<int> indexes = new List<int>();
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            char c = line[i];
+            if (!char.IsLetterOrDigit(c) && c != char.Parse("."))
+                indexes.Add(i);
+        }
+
+        return indexes.ToArray();
+    }
 }
