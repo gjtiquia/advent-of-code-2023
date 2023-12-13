3ec54f33456d1925bb5aa9b54c74c7f411418bf1
GJ Tiquia <gershom.tiquia@gmail.com>
2023-12-13 17:33:55 +0800

    feat: finish part 1 tests

--- UnitTests/Day7/Day7Tests.cs
+++ UnitTests/Day7/Day7Tests.cs
@@ -9,6 +9,9 @@ public class Day7Tests
     [TestCase("23432", EHandType.TwoPair)]
     [TestCase("A23A4", EHandType.OnePair)]
     [TestCase("23456", EHandType.HighCard)]
+
+    [TestCase("T55J5", EHandType.ThreeOfAKind)]
+    [TestCase("KTJJT", EHandType.TwoPair)]
     public void ShouldDetermineTheTypeCorrectly(string line, EHandType expectedType)
     {
         Hand hand = new Hand(line);
@@ -18,21 +21,60 @@ public class Day7Tests
     [TestCase("AAAAA", "AA8AA")]
     [TestCase("33332", "2AAAA")]
     [TestCase("77888", "77788")]
+    [TestCase("T55J5", "KTJJT")]
     public void ShouldCompareCorrectly(string strongerLine, string weakerLine)
     {
         Hand strongerHand = new Hand(strongerLine);
         Hand weakerHand = new Hand(weakerLine);
 
-        List<Hand> hands = [strongerHand, weakerHand];
+        Assert.That(strongerHand.CompareTo(weakerHand), Is.GreaterThan(0));
+    }
 
-        // Sorts weak to strong, ascending order
-        hands.Sort();
+    [Test]
+    public void ShouldParseAndCalculateWinnings()
+    {
+        // Specified in .csproj to include and copy to bin folder where the test is executed
+        string currentDirectory = TestContext.CurrentContext.TestDirectory;
+        string filePath = Path.Combine(currentDirectory, "Day7", "input.txt");
 
-        Assert.That(hands[0], Is.EqualTo(weakerHand));
+        string[] lines = File.ReadAllLines(filePath);
+        int winnings = Day7Parser.CalculateWinnings(lines);
+
+        Assert.That(winnings, Is.EqualTo(6440));
     }
 }
 
-public class Hand : IComparable
+public static class Day7Parser
+{
+    public static int CalculateWinnings(string[] lines)
+    {
+        SortedDictionary<Hand, int> sortedHandsAndBids = [];
+
+        foreach (string line in lines)
+        {
+            string[] splitLines = Utilities.SplitAndTrim(" ", line);
+
+            Hand hand = new Hand(splitLines[0]);
+            int bid = int.Parse(splitLines[1]);
+
+            sortedHandsAndBids.Add(hand, bid);
+        }
+
+        int totalWinnings = 0;
+
+        int currentRank = 0;
+        foreach (var (_, bid) in sortedHandsAndBids)
+        {
+            currentRank++;
+
+            totalWinnings += currentRank * bid;
+        }
+
+        return totalWinnings;
+    }
+}
+
+public class Hand : IComparable<Hand>
 {
     private string _hand;
 
@@ -101,18 +143,20 @@ public class Hand : IComparable
         return EHandType.HighCard;
     }
 
-    public int CompareTo(object? obj)
+    public int CompareTo(Hand? obj)
     {
-        if (obj is not Hand)
+        if (obj == null || obj is not Hand)
             return 1;
 
         Hand otherHand = (Hand)obj;
 
-        EHandType thisHandType = GetHandType();
+        EHandType thisHandType = this.GetHandType();
         EHandType otherHandType = otherHand.GetHandType();
 
         if ((int)thisHandType < (int)otherHandType)
             return 1;
+        else if ((int)thisHandType > (int)otherHandType)
+            return -1;
 
         for (int i = 0; i < _hand.Length; i++)
         {
@@ -133,6 +177,7 @@ public class Hand : IComparable
 
         return 0;
     }
+
 
     private int GetCharRanking(char c)
     {
